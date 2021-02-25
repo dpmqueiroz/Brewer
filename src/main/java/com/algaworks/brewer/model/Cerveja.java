911c502c9a8e084af7586499b6cd58678dd0f17a
Daniel Queiroz <d.queiroz@df.estudante.senai.br>
2021-02-25 10:33:50 -0300

    Aula 8.3 CONCLUIDA

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,9 +1,52 @@
 package com.algaworks.brewer.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.ibm.icu.math.BigDecimal;
+
+@Entity
+@Table(name = "cerveja")
 public class Cerveja {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 
 	@javax.validation.constraints.NotBlank(message = "SKU é obrigatório")
 	private String sku;
@@ -15,6 +58,89 @@ public class Cerveja {
 	@Size(min = 3, max = 200, message = "A descrição deve conter entre 3 e 300 caracteres")
 	private String descricao;
 	
+	private BigDecimal valor;
+	
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+	
+	private BigDecimal comissao;
+	
+	@Column(name = "quantidade_estoque")
+	private Integer quantidadeEstoque;
+	
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+	
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")
+	private Estilo Estilo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Origem getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+
+	public Sabor getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+
+	public Estilo getEstilo() {
+		return Estilo;
+	}
+
+	public void setEstilo(Estilo estilo) {
+		Estilo = estilo;
+	}
 
 	public String getSku() {
 		return sku;
